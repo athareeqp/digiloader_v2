87209e5774338a552ea9942e01114e8065bab6d9
Aulya Thareeq <aulyathareeq@gmail.com>
2022-12-01 15:17:06 +0700

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,10 +5,10 @@ export default function About() {
   return (
     <div className="about grid-container">
       <h1>About App</h1>
-      <p>DigiLoader is an app that contains some information about Digimon Card. This app was made as my Final Assignment of Mobile Device Programming. Twas created using React JS and a Digimon TCG API.</p>
+      <p>DigiLoader is an app that contains some information about Digimon Card. This app was made as my Final Assignment of Practical Mobile Device Programming. Twas created using React JS and a Digimon TCG API.</p>
       <h2>About Author</h2>
       <img src="foto.jpg" alt="author pic" />
-      <p>The name's Luey, short for Aulya Thareeq Pravantawidya. By the time I made this app I am on my 5th semester</p>
+      <p>The name's Luey, short for Aulya Thareeq Pravantawidya. By the time I made this app I am on my 5th semester in Computer Engineering, Diponegoro University.</p>
     </div>
   );
 }
